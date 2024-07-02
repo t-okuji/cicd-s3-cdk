5fef80d96d6f72b89307a6ab03a3d7adee41bcba
t-okuji <t.okuji34@gmail.com>
2024-07-02 17:03:53 +0900

    feat: first init

---
+++ test/cicd-s3-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CicdS3Cdk from '../lib/cicd-s3-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cicd-s3-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CicdS3Cdk.CicdS3CdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
