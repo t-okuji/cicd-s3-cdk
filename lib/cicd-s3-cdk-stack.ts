f203c4915eb42e24397fa02c9d0f46c842fa9e4c
t-okuji <t.okuji34@gmail.com>
2024-07-04 11:06:01 +0900

    feat: Create cicd cdk code

--- lib/cicd-s3-cdk-stack.ts
+++ lib/cicd-s3-cdk-stack.ts
@@ -1,16 +1,95 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as codecommit from "aws-cdk-lib/aws-codecommit";
+import * as codebuild from "aws-cdk-lib/aws-codebuild";
+import * as ecr from "aws-cdk-lib/aws-ecr";
+import * as codepipeline from "aws-cdk-lib/aws-codepipeline";
+import * as codepipeline_actions from "aws-cdk-lib/aws-codepipeline-actions";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 export class CicdS3CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const resourceName = "cicd-s3";
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CicdS3CdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // CodeCommit repository
+    const codeRepository = new codecommit.Repository(this, "CodeRepository", {
+      repositoryName: `${resourceName}-code-repository`,
+    });
+
+    // Ecr repository for codebuild custom image
+    const ecrRepository = new ecr.Repository(this, "EcrRepository", {
+      repositoryName: `${resourceName}-ecr-repository`,
+    });
+
+    // CodeBuild
+    const buildProject = new codebuild.PipelineProject(this, `BuildProject`, {
+      projectName: `${resourceName}-code-build`,
+      buildSpec: codebuild.BuildSpec.fromSourceFilename("buildspec.yml"),
+      environment: {
+        buildImage: codebuild.LinuxBuildImage.fromEcrRepository(ecrRepository),
+        computeType: codebuild.ComputeType.SMALL,
+      },
+      environmentVariables: {
+        S3_BUCKET_NAME: {
+          type: codebuild.BuildEnvironmentVariableType.PLAINTEXT,
+          value: process.env.S3_BUCKET_NAME ?? "",
+        },
+      },
+    });
+
+    // CodeBuild role
+    buildProject.addToRolePolicy(
+      new iam.PolicyStatement({
+        resources: [
+          `arn:aws:s3:::${process.env.S3_BUCKET_NAME ?? ""}`,
+          `arn:aws:s3:::${process.env.S3_BUCKET_NAME ?? ""}/*`,
+        ],
+        actions: [
+          "s3:DeleteObject",
+          "s3:GetBucketLocation",
+          "s3:GetObject",
+          "s3:ListBucket",
+          "s3:PutObject",
+        ],
+      })
+    );
+
+    // Artifact
+    const sourceOutput = new codepipeline.Artifact(`source_artifact`);
+    const buildOutput = new codepipeline.Artifact(`build_output`);
+
+    // CodePipeline Actions
+    const sourceAction = new codepipeline_actions.CodeCommitSourceAction({
+      actionName: "CodeCommit",
+      repository: codeRepository,
+      branch: "main",
+      output: sourceOutput,
+    });
+    const buildAction = new codepipeline_actions.CodeBuildAction({
+      actionName: "CodeBuild",
+      project: buildProject,
+      input: sourceOutput,
+      outputs: [buildOutput],
+    });
+
+    // CodePipeline
+    new codepipeline.Pipeline(this, "CodePipeline", {
+      pipelineName: `${resourceName}-code-pipeline`,
+      stages: [
+        {
+          stageName: "Source",
+          actions: [sourceAction],
+        },
+        {
+          stageName: "Build",
+          actions: [buildAction],
+        },
+      ],
+    });
   }
 }
